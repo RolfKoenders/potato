d53a5c45a2affa3dad0744cf8939275cbe1a9074
Rolf Koenders <post@madebyrolf.nl>
2016-08-30 22:44:42 +0200

    Remove prototype code. Make the bot actually run

--- bin/bot.js
+++ bin/bot.js
@@ -8,17 +8,5 @@
 let config = require('../config');
 let CouchpotatoBot = require('../lib/couchpotato-bot');
 
-// let bot = new CouchpotatoBot(config.getProperties());
-// bot.run();
-
-var OmdbAPI = require('../lib/Omdb');
-
-var api = new OmdbAPI();
-api.get({
-	movieTitle: 'frozen'
-}, (err, movies) => {
-	if (err) {
-		return console.log(err);
-	}
-	console.log('Found movies \n ', movies);
-});
+let bot = new CouchpotatoBot(config.getProperties());
+bot.run();
