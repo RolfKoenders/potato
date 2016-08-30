0962e87fb26fe055dc88a1e82d54856ef547d5d6
Rolf Koenders <post@madebyrolf.nl>
2016-08-30 21:45:00 +0200

    Structured bot code. Added omdb dep

--- bin/bot.js
+++ bin/bot.js
@@ -10,16 +10,23 @@ let CouchpotatoBot = require('../lib/couchpotato-bot');
 const TOKEN = process.env.BOT_API_KEY || require('../token');
 const NAME = process.env.BOT_NAME || 'Coupo';
 
-let bot = new CouchpotatoBot({
-    bot: {
-        token: TOKEN,
-        name: NAME
-    },
-    couch: {
-        host: '',
-        port: '',
-        key: ''
-    }
-});
+// let bot = new CouchpotatoBot({
+//     bot: {
+//         token: TOKEN,
+//         name: NAME
+//     }
+// });
+//
+// bot.run();
 
-bot.run();
+
+var OmdbAPI = require('../lib/Omdb');
+
+OmdbAPI.search({
+    movieTitle: 'frozen'
+}, (err, movies) => {
+    if(err) {
+        return console.log(err);
+    }
+    console.log('Found movies \n ', movies);
+});
