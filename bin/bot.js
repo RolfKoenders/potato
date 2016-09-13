127399dcc8545a20d581f3786b48fde1140a3be7
Rolf Koenders <post@madebyrolf.nl>
2016-09-14 01:18:31 +0200

    Feature/split bot package (#3)
    * Split handlers from bot
    
    * Use the new slackbotify package.
    
    * Update package.json

--- bin/bot.js
+++ bin/bot.js
@@ -5,8 +5,50 @@
  * Couchpotato SlackBot launch script.
 */
 
+const Bot = require('slackbotify');
 const config = require('../config');
-const CouchpotatoBot = require('../lib/bot');
+const MessageHandlers = require('../lib/handlers');
 
-let bot = new CouchpotatoBot(config.getProperties());
+let bot = new Bot(config.getProperties());
+
+bot.registerHandler({
+	group: 'channel',
+	match: /download movie\s(.*)/,
+	handler: MessageHandlers.downloadMovieHandler
+});
+
+bot.registerHandler({
+	group: 'channel',
+	match: /search movie\s(.*)/,
+	handler: MessageHandlers.searchMovieHandler
+});
+
+bot.registerHandler({
+	group: 'direct',
+	match: /hi/ig,
+	handler: function (message, callback) {
+		callback('Hi there! :smiley:');
+	}
+});
+
+bot.registerHandler({
+	group: 'direct',
+	match: /:popcorn:/ig,
+	handler: function (message, callback) {
+		callback('POPCORN! :yum:');
+	}
+});
+
+bot.registerHandler({
+	group: 'direct',
+	match: 'help',
+	handler: MessageHandlers.helpHandler
+});
+
+bot.registerHandler({
+	group: 'admin',
+	match: /!version/i,
+	handler: MessageHandlers.couchPotatoHandler
+});
+
 bot.run();
